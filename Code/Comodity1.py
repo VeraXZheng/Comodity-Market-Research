aac3775a8a48889bc0d67fce9a13ee27a8a91d4e
Vera <vera982@live.com>
2020-07-27 10:38:46 +0200

    updated code

--- Code/Comodity1.py
+++ Code/Comodity1.py
@@ -65,7 +65,7 @@ class ProcessOneFactor:
                dSigma = alpha *sigma[i,j] * dw2[i,j] 
                sigma[i+1,j] = sigma[i,j] + dSigma
                St[i+1,j] = St[i,j] + (1-St[i,j])* a * dt + sigma[i,j] * St[i,j]  * dw1[i,j]
-          
+        
 
         return St, sigma
    
@@ -114,36 +114,19 @@ class ProcessOneFactor:
 
         Call = np.exp(-r*T)* np.sum(call)/m
         Put = np.exp(-r*T)* np.sum(put)/m
-        return Call, Put
+        SE_call = np.sqrt(sum(call-Call)/(m*(m-1)))
+        SE_put = np.sqrt(sum(put-Put)/(m*(m-1)))
+        return Call, Put, SE_call, SE_put
         
  ###############main############
 class AsianBasketOption:
-    def __init__(self, sigma1,sigma2,F1_0,F2_0,rho,m,k,weight):
-     
-        self._sigma1 = sigma1
-        self._sigma2 = sigma2
-        self._F1_0 = F1_0
-        self._F2_0 = F2_0
-        self._rho = rho
-        self._m = m
+    def __init__(self,k,weight):
+       
         self._k = k 
         self._weight = weight
-    def simulate(self,T, dt):
-        F1_0 = self._F1_0
-        F2_0 = self._F2_0
-        rho = self._rho
-        sigma1 = self._sigma1 
-        sigma2 = self._sigma2 
-        n = round(T / dt)
-        F1t = np.zeros((n,m))
-        F2t = np.zeros((n,m))
+   
       
-        for i in range(m):
-            dw1 = np.random.normal(size= n)*np.sqrt(dt)
-            dw2 = np.random.normal(size = n)*np.sqrt(dt)
-            F1t[:,i] = F1_0 * np.cumprod(np.exp((-0.5*sigma1 **2)*dt+sigma1*dw1),0)
-            F2t[:,i] = F2_0 * np.cumprod(np.exp((-0.5*sigma2 **2)*dt+sigma2 * (rho*dw1+np.sqrt(1-rho**2)*dw2)),0)
-        return F1t, F2t
+       
     
     def pricing(self,n1,n2,F1t,F2t):
         #[n1,n2]is the interval to take averge of the future prices
@@ -164,52 +147,67 @@ class AsianBasketOption:
    ###########################################################
 ### start main
 if __name__ == "__main__":
-    T = 10
-    dt = 0.05
+    output_path = "/Users/veraisice/Desktop/Comodity-Market-Research/Output/"
+    input_path  = "/Users/veraisice/Desktop/Comodity-Market-Research/Input/"
+    T = 1
+    dt = 1/250
     sigma0 = 0.5
     alpha = 0.5
     rho = 0.5
-    m = 1
+    m = 1000
     K0 =1
     r =0.01
     #list of mean reverting speed of interest
     alist=[0,0.5,1]
+    styl_list=['-','--','-.']
     # plot of the fictitious spot prices dynamics
-    plt.figure()
-    for a in alist:
-        S,sigma = ProcessOneFactor(sigma0,a, m, alpha, rho).Simulate(T, dt)
-        
-        plt.plot(S, label=r"OneFactor(a =" + str(a) +", $\sigma_0$ ="+ str(sigma0)+")")
+    plt.figure(dpi=1200)
+    for i in range(len(alist)):
+        S,sigma = ProcessOneFactor(sigma0,alist[i], m, alpha, rho).Simulate(T, dt)
+        avgS = np.mean(S,1)
+        plt.plot(avgS, ls =styl_list[i], label=r"OneFactor(a =" + str(alist[i]) +", $\sigma_0$ ="+ str(sigma0)+")")
        
     plt.xlabel('Time index')
     plt.ylabel('Value')
     plt.title("Realization of fictious spot prices dynamics")
     plt.legend(loc='best')
+    #plt.savefig(output_path + "Figures/OneFactor_spot")
    # plot of the futures prices dynamics 
-    plt.figure()
-    for a in alist:
-            St,sigma = ProcessOneFactor(sigma0, a,m, alpha, rho).Simulate(T, dt)
-            plt.plot(ProcessOneFactor(sigma,a,m, alpha, rho).FutureDynamics(T,dt,St,F0=1),label=r"OneFactor(a = "+ str(a) +", $\sigma_0$ = "+ str(sigma0)+")")
+    plt.figure(dpi=1200)
+    for i in range(len(alist)):
+            St,sigma = ProcessOneFactor(sigma0, alist[i],m, alpha, rho).Simulate(T, dt)
+            Ft = ProcessOneFactor(sigma0,alist[i],m, alpha, rho).FutureDynamics(T,dt,St,F0=1)
+            avgFt =  np.mean(Ft,1)
+            plt.plot(avgFt,ls =styl_list[i],label=r"OneFactor(a = "+ str(alist[i]) +", $\sigma_0$ = "+ str(sigma0)+")")
     
     plt.xlabel('Time index')
     plt.ylabel('Value')
     plt.title("Realization of Future processes")
     plt.legend(loc='best')
-
+    #plt.savefig(output_path + "Figures/OneFactor_futures")
 #call and put prices
     call = np.zeros(len(alist))
     put = np.zeros(len(alist))
+    SE_call = np.zeros(len(alist))
+    SE_put = np.zeros(len(alist))
     for i in range(len(alist)):
         St,sigma = ProcessOneFactor(sigma0, alist[i],m, alpha, rho).Simulate(T, dt)
-        call[i], put[i] = ProcessOneFactor(sigma0, alist[i],m, alpha, rho).OptionPricing(St, K0, r, dt, T)
-    print ("Call and put option prices by MC method with" +str(m)+ " simulations and different MR speed are " + str(call) + " and "+str(put)+" respectively")
+        
+        call[i], put[i],SE_call[i], SE_put[i] = ProcessOneFactor(sigma0, alist[i],m, alpha, rho).OptionPricing(St, K0, r, dt, T)
+       
+    print ("Call and put option prices by MC method with" +str(m)+ " simulations and different MR speed are " + str(call) + " and "+str(put)+\
+           " respectively; with Standard Error for Call " +str(SE_call)+ " and Put "+str(SE_put))
            
            
 ###################Asian Basket Option#########
     m=1000
-    F1t,F2t = AsianBasketOption(sigma1=0.3, sigma2=0.4, F1_0=100, F2_0=80, rho=0.5, m=1000, k=88, weight=(0.4,0.6)).simulate(1,1/250)     
-    V, avgV = AsianBasketOption(sigma1=0.3, sigma2=0.4, F1_0=100, F2_0=80, rho=0.5, m=1000, k=88, weight=(0.4,0.6)).pricing(100, 200, F1t, F2t)    
-    print('Call price of the Asian basket Option with ' + str(m)+" simulations is " +str(avgV))     
+    sigma1 =0.4
+    sigma2 = 0.6
+    for i in range(len(alist)):
+        F1t = ProcessOneFactor(sigma1,alist[i],m, 0.2, 0.2).FutureDynamics(T,dt,St,F0=1)
+        F2t = ProcessOneFactor(sigma2,alist[i],m, 0.2, 0.3).FutureDynamics(T,dt,St,F0=3)
+        V, avgV = AsianBasketOption(k=2.2, weight=(0.4,0.6)).pricing(100, 200, F1t, F2t)    
+        print('Call price of the Asian basket Option with ' + str(m)+" simulations is " +str(avgV))     
            
            
-           
+    Future_prices = pd.read_excel(input_path+ "1M_ForwardCurve"+".xlsx")     
